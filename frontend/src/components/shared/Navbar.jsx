6d3a414b2c81abf8b00aef4d1391dbbb483d4752
wasiahmad <alexxwasi95@gmail.com>
2025-03-07 21:23:27 +0530

    Responsive update

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,25 +1,154 @@
-import React from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { Button } from "../ui/button";
-import { Avatar, AvatarImage } from "../ui/avatar";
-import { LogOut, User2 } from "lucide-react";
+// import React from "react";
+// import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+// import { Button } from "../ui/button";
+// import { Avatar, AvatarImage } from "../ui/avatar";
+// import { LogOut, User2 } from "lucide-react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import axios from "axios";
+// import { USER_API_END_POINT } from "@/utils/constant";
+// import { setUser } from "@/redux/authSlice";
+// import { toast } from "sonner";
+
+// const Navbar = () => {
+//   const { user } = useSelector((store) => store.auth);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const logoutHandler = async () => {
+//     try {
+//       const res = await axios.get(`${USER_API_END_POINT}/logout`, {
+//         withCredentials: true,
+//       });
+//       if (res.data.success) {
+//         dispatch(setUser(null));
+//         navigate("/");
+//         toast.success(res.data.message);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       toast.error(error.response.data.message);
+//     }
+//   };
+//   return (
+//     <div className="bg-white">
+//       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+//         <div>
+//           <h1 className="text-2xl font-bold">
+//             Job<span className="text-[#2E86C1]">Hive</span>
+//           </h1>
+//         </div>
+//         <div className="flex items-center gap-12">
+//           <ul className="flex font-medium items-center gap-5">
+//             {user && user.role === "recruiter" ? (
+//               <>
+//                 <li>
+//                   <Link to="/admin/companies">Companies</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/admin/jobs">Jobs</Link>
+//                 </li>
+//               </>
+//             ) : (
+//               <>
+//                 <li>
+//                   <Link to="/">Home</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/jobs">Jobs</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/browse">Browse</Link>
+//                 </li>
+//               </>
+//             )}
+//           </ul>
+//           {!user ? (
+//             <div className="flex items-center gap-2">
+//               <Link to="/login">
+//                 <Button variant="outline">Login</Button>
+//               </Link>
+//               <Link to="/signup">
+//                 <Button className="bg-[#2E86C1] hover:bg-[#2E96C3]">
+//                   Signup
+//                 </Button>
+//               </Link>
+//             </div>
+//           ) : (
+//             <Popover>
+//               <PopoverTrigger asChild>
+//                 <Avatar className="cursor-pointer">
+//                   <AvatarImage
+//                     src={user?.profile?.profilePhoto}
+//                     alt="@shadcn"
+//                   />
+//                 </Avatar>
+//               </PopoverTrigger>
+//               <PopoverContent className="w-80">
+//                 <div className="">
+//                   <div className="flex gap-2 space-y-2">
+//                     <Avatar className="cursor-pointer">
+//                       <AvatarImage
+//                         src={user?.profile?.profilePhoto}
+//                         alt="@shadcn"
+//                       />
+//                     </Avatar>
+//                     <div>
+//                       <h4 className="font-medium">{user?.fullname}</h4>
+//                       <p className="text-sm text-muted-foreground">
+//                         {user?.profile?.bio}
+//                       </p>
+//                     </div>
+//                   </div>
+//                   <div className="flex flex-col my-2 text-gray-600">
+//                     {user && user.role === "student" && (
+//                       <div className="flex w-fit items-center gap-2 cursor-pointer">
+//                         <User2 />
+//                         <Button variant="link">
+//                           {" "}
+//                           <Link to="/profile">View Profile</Link>
+//                         </Button>
+//                       </div>
+//                     )}
+
+//                     <div className="flex w-fit items-center gap-2 cursor-pointer">
+//                       <LogOut />
+//                       <Button onClick={logoutHandler} variant="link">
+//                         Logout
+//                       </Button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </PopoverContent>
+//             </Popover>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
-import { USER_API_END_POINT } from "@/utils/constant";
+import { LogOut, Menu } from "lucide-react";
 import { setUser } from "@/redux/authSlice";
 import { toast } from "sonner";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
 
 const Navbar = () => {
   const { user } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const logoutHandler = async () => {
     try {
-      const res = await axios.get(`${USER_API_END_POINT}/logout`, {
-        withCredentials: true,
-      });
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
       if (res.data.success) {
         dispatch(setUser(null));
         navigate("/");
@@ -27,104 +156,28 @@ const Navbar = () => {
       }
     } catch (error) {
       console.log(error);
-      toast.error(error.response.data.message);
+      toast.error("Logout failed");
     }
   };
-  return (
-    <div className="bg-white">
-      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
-        <div>
-          <h1 className="text-2xl font-bold">
-            Job<span className="text-[#2E86C1]">Hive</span>
-          </h1>
-        </div>
-        <div className="flex items-center gap-12">
-          <ul className="flex font-medium items-center gap-5">
-            {user && user.role === "recruiter" ? (
-              <>
-                <li>
-                  <Link to="/admin/companies">Companies</Link>
-                </li>
-                <li>
-                  <Link to="/admin/jobs">Jobs</Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/jobs">Jobs</Link>
-                </li>
-                <li>
-                  <Link to="/browse">Browse</Link>
-                </li>
-              </>
-            )}
-          </ul>
-          {!user ? (
-            <div className="flex items-center gap-2">
-              <Link to="/login">
-                <Button variant="outline">Login</Button>
-              </Link>
-              <Link to="/signup">
-                <Button className="bg-[#2E86C1] hover:bg-[#2E96C3]">
-                  Signup
-                </Button>
-              </Link>
-            </div>
-          ) : (
-            <Popover>
-              <PopoverTrigger asChild>
-                <Avatar className="cursor-pointer">
-                  <AvatarImage
-                    src={user?.profile?.profilePhoto}
-                    alt="@shadcn"
-                  />
-                </Avatar>
-              </PopoverTrigger>
-              <PopoverContent className="w-80">
-                <div className="">
-                  <div className="flex gap-2 space-y-2">
-                    <Avatar className="cursor-pointer">
-                      <AvatarImage
-                        src={user?.profile?.profilePhoto}
-                        alt="@shadcn"
-                      />
-                    </Avatar>
-                    <div>
-                      <h4 className="font-medium">{user?.fullname}</h4>
-                      <p className="text-sm text-muted-foreground">
-                        {user?.profile?.bio}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex flex-col my-2 text-gray-600">
-                    {user && user.role === "student" && (
-                      <div className="flex w-fit items-center gap-2 cursor-pointer">
-                        <User2 />
-                        <Button variant="link">
-                          {" "}
-                          <Link to="/profile">View Profile</Link>
-                        </Button>
-                      </div>
-                    )}
 
-                    <div className="flex w-fit items-center gap-2 cursor-pointer">
-                      <LogOut />
-                      <Button onClick={logoutHandler} variant="link">
-                        Logout
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </PopoverContent>
-            </Popover>
+  return (
+    <nav className="bg-white shadow-md p-4 sticky top-0 w-full z-50">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl font-bold">JobPortal</Link>
+        <button className="lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          <Menu size={28} />
+        </button>
+        <div className={`lg:flex space-x-6 ${menuOpen ? "block" : "hidden"} lg:block absolute lg:relative top-16 lg:top-0 left-0 bg-white w-full lg:w-auto p-4 lg:p-0 shadow-lg lg:shadow-none` }>
+          <Link to="/jobs" className="block p-2 lg:inline-block">Jobs</Link>
+          <Link to="/companies" className="block p-2 lg:inline-block">Companies</Link>
+          {user ? (
+            <button onClick={logoutHandler} className="block p-2 lg:inline-block">Logout</button>
+          ) : (
+            <Link to="/login" className="block p-2 lg:inline-block">Login</Link>
           )}
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
